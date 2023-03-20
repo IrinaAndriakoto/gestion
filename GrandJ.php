47341eb7113fa12babb9e47ceecfdbbe92491415
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2023-03-20 18:28:13 +0000

    Add files via upload

---
+++ GrandJ.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" href="pngtree-vector-money-saving-icon-png-image_1014747.jpg" type="image/x-icon">
+	<title>Page avec barre de recherche et tableau</title>
+	<style>
+		/* Styles pour la barre de menu */
+		.barre-menu {
+			background-color: green;
+			padding: 10px;
+			color: white;
+			font-size: 18px;
+			font-weight: bold;
+			font-family: "Segoe UI", "Helvetica Neue", sans-serif;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			border-radius: 10px;
+		}
+
+		/* Styles pour les en-têtes de tableau */
+		table th {
+			background-color: yellow;
+			padding: 10px;
+			text-align: left;
+			font-weight: bold;
+			font-family: "Segoe UI", "Helvetica Neue", sans-serif;
+		}
+
+		/* Styles pour le tableau */
+		table {
+			margin: 0 auto;
+			width: 50%;
+			border-collapse: collapse;
+			font-family: "Segoe UI", "Helvetica Neue", sans-serif;
+		}
+
+		table td, table th {
+			border: 1px solid black;
+			padding: 10px;
+		}
+	</style>
+</head>
+<body>
+	<div class="barre-menu">
+		<div>Barre de menu</div>
+		<form action="recherche.php" method="get">
+			<label for="recherche" style="color:white;">Recherche :</label>
+			<input type="text" id="recherche" name="recherche" style="margin-left: 10px;">
+			<button type="submit">Rechercher</button>
+		</form>
+	</div>
+
+	<!-- Titre de la page -->
+	<h1>Grand Journal</h1>
+
+	<!-- Tableau -->
+	<table>
+		<tr>
+			<th>idGrandj</th>
+			<th>codeJournal</th>
+            <th>date</th>
+            <th>numero Pieces</th>
+            <th>References Pieces</th>
+            <th>Numero Compte</th>
+            <th>Libelle Ecriture</th>
+            <th>Devise</th>
+            <th>Parite</th>
+            <th>Quantite</th>
+            <th>Montant devise</th>
+            <th>Prix Unitaire</th>
+            <th>Debit</th>
+            <th>Credit</th>
+		</tr>
+		<tr>
+			<td>Ligne 1, colonne 1</td>
+			<td>Ligne 1, colonne 2</td>
+		</tr>
+		<tr>
+			<td>Ligne 2, colonne 1</td>
+			<td>Ligne 2, colonne 2</td>
+		</tr>
+	</table>
+</body>
+</html>
